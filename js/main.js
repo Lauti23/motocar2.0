6583c011a68b7d0d4e99d3cec73a07956ff6f9e9
Lautaro Vazquez <lauti1498@gmail.com>
2022-06-30 21:58:35 -0300

    2da entrega

--- js/main.js
+++ js/main.js
@@ -1,208 +1,90 @@
-let alCarrito = document.getElementsByTagName("button");
-console.log(alCarrito);
+const stockMotos = [{id:1 ,producto: "CRF 300L", precio: 659375, imagen: 'motocar2.0/assets/motos/honda/CRF250R.jpg', stock: 1},
+                {id:2 ,producto: "CRF 250R", precio: 1102960, imagen:'motocar2.0/assets/motos/honda/crf300l.jpg', stock: 1},
+                {id:3 ,producto: "CRF 450R", precio: 1162900, imagen: 'motocar2.0/assets/motos/honda/CRF450R.jpg', stock: 1},
+                {id:4 ,producto: "KLX 250L", precio: 548482, imagen: 'motocar2.0/assets/motos/kawasaki/klx250l.jpg', stock: 1},
+                {id:5 ,producto: "KX 250F", precio: 1060990, imagen: 'motocar2.0/assets/motos/kawasaki/kx250f.jpg', stock: 1},
+                {id:6 ,producto: "KX 450F", precio: 1126936, imagen: 'motocar2.0/assets/motos/kawasaki/kx450f.jpg', stock: 1},                   
+                {id:7 ,producto: "EXC - F350", precio: 1192380, imagen: 'motocar2.0/assets/motos/ktm/exc-f350.jpg', stock: 1},
+                {id:8 ,producto: "SX 125", precio: 1039420, imagen: 'motocar2.0/assets/motos/ktm/sx125.jpg', stock: 1},
+                {id:9 ,producto: "SX 450", precio: 1322352, imagen: 'motocar2.0/assets/motos/ktm/sx450.jpg', stock: 1},
+                {id:10 ,producto: "RM-Z 250", precio: 1001055, imagen: 'motocar2.0/assets/motos/suzuki/rm-z250.jpg', stock: 1},
+                {id:11 ,producto: "RM-Z 450", precio: 1018670, imagen: 'motocar2.0/assets/motos/suzuki/rm-z450.jpg', stock: 1},
+                {id:12 ,producto: "RM-Z 450 2017", precio: 959095, imagen: 'motocar2.0/assets/motos/suzuki/rm-z4502017.jpg', stock: 1},
+                {id:13 ,producto: "WR 450", precio: 1198870, imagen: 'motocar2.0/assets/motos/yamaha/wr450f.jpg', stock: 1},
+                {id:14 ,producto: "YZ 250", precio: 995060, imagen: 'motocar2.0/assets/motos/yamaha/yz250.jpg', stock: 1},
+                {id:15 ,producto: "YZ 450", precio: 1155095, imagen: 'motocar2.0/assets/motos/yamaha/yz450f.jpg', stock: 1},];                 
+
+const carrito = [];
+
+const enviarAlCarrito = document.querySelectorAll('#botonComprar');
+
+// console.log(enviarAlCarrito);
+
+enviarAlCarrito.forEach((enviadoAlCarrito) => {
+    enviadoAlCarrito.addEventListener('click', alCarritoClick);
+    })
+
+//FUNCION CON HTML
+function alCarritoClick (event) {
+const boton = event.target;
+const item = boton.closest('.section__tarjeta');
+const itemImg = item.querySelector('.section__tarjeta-imagen').src;
+const itemTitulo = item.querySelector('.section__tarjeta-titulo').textContent;
+const itemPrecio = item.querySelector('.section__tarjeta-precio').textContent;
+listoEnCarrito(itemImg, itemTitulo, itemPrecio);
+}
 
 
+function listoEnCarrito (itemImg, itemTitulo, itemPrecio) {
+    const filaCarrito = document.createElement('div');
+    const carritoCont = document.querySelector('#carrito__item');
+    const carritoContenido =    `<div class="carrito__elemento">
+                                    <div id="carritoImg" class="carrito__imagen">
+                                        <img class="carrito__imagen-foto" src=${itemImg} alt="">
+                                    </div>
+                                    <div id="carritoProducto" class="carrito__producto">
+                                        <p class="carrito__modelo">${itemTitulo}</p>
+                                    </div>
+                                    <div class="carrito__precio">
+                                        <p id="carritoPrecio">${itemPrecio}</p>
+                                    </div> 
+                                    <div class="carrito__eliminar">
+                                        <button type="button">X</button>
+                                    </div>
+                                </div>`
+    filaCarrito.innerHTML = carritoContenido;
+    carritoCont.append(filaCarrito);
 
+    filaCarrito.querySelector('.carrito__eliminar').addEventListener('click', borrarMoto)
 
+precioFinal();
+}
 
+function precioFinal () {
+    let total = 0;
 
+    const precioFinalCarrito = document.querySelector('#total');
 
+    const motosEnCarrito = document.querySelectorAll('.carrito__elemento');
 
+    motosEnCarrito.forEach((carritoMoto) => {
+        const motosEnCarritoPrecio = carritoMoto.querySelector('carrito__precio');
 
+        const motosPrecios = Number(motosEnCarritoPrecio.textContent.replace('$', ''));
+        total = total + motosPrecios;
+    })
+    precioFinalCarrito.innerHTML = `$${total}`
+}
 
+function borrarMoto(event) {
+    const botonBorrar = event.target;
+    botonBorrar.closest ('.carrito__elemento').remove();
+    precioFinal();
+}
 
+function comprarAuto() {
+    carritoCont.innerHTML = '';
+    alert("Gracias por su compra");
+    precioFinal();
+}
 
-
-
-
-
-
-
-
-
-
-
-// class Moto {
-//     constructor (marca, modelo, precio) {
-//         this.marca = marca;
-//         this.modelo = modelo;
-//         this.precio = Number(precio);
-//     }
-// }
-
-// const motos = [];
-// motos.push(new Moto ("Honda", "CRF 300L", 659375));
-// motos.push(new Moto ("Honda", "CRF 250R", 1102960));
-// motos.push(new Moto ("Honda", "CRF 450R", 1162900));
-// motos.push(new Moto ("Kawasaki", "KLX 250L", 548482));
-// motos.push(new Moto ("Kawasaki", "KX 250F", 1060990));
-// motos.push(new Moto ("Kawasaki", "KX 450F", 1126936));
-// motos.push(new Moto ("KTM", "EXC - F350", 1192380));
-// motos.push(new Moto ("KTM", "SX 125", 1039420));
-// motos.push(new Moto ("KTM", "SX 450", 1322352));
-// motos.push(new Moto ("Suzuki", "RM-Z 250", 1001055));
-// motos.push(new Moto ("Suzuki", "RM-Z 450", 1018670));
-// motos.push(new Moto ("Suzuki", "RM-Z 450 2017", 959095));
-// motos.push(new Moto ("Yamaha", "WR 450", 1198870));
-// motos.push(new Moto ("Yamaha", "YZ 250", 995060));
-// motos.push(new Moto ("Yamaha", "YZ 450", 1155095));
-
-// comprarMoto.addEventListener('click', enviarCarrito);
-// function enviarCarrito() {
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let precio;
-// let total = 0;
-// let repetir = false;
-// let nombre = prompt("Hola, bienvenido a Motocar Repuestos. ¿Cómo es tu nombre?");
-// const compraDeMoto = () => {
-//     let motocross;
-//     precio = 0;
-//     do {
-//         motocross = parseInt(prompt(`¿Qué moto desea comprar ${nombre}? Coloque el número que corresponda a la marca\n1- Honda\n2- Kawasaki\n3- KTM\n4- Suzuki\n5- Yamaha`)); 
-        
-//         switch (motocross) {
-            
-//             case 1:
-//                 motocross = prompt("¿Qué modelo quéres?\n1- CRF 300L\n2- CRF 250R\n3-CRF 450R");
-//                 if (motocross == 1) {
-//                     motocross = motos[0].modelo;
-//                     precio = motos[0].precio;
-                    
-//                 } else if (motocross == 2) {
-//                     motocross = motos[1].modelo;
-//                     precio = motos[1].precio;
-                    
-//                 } else if (motocross == 3) {
-//                     motocross = motos[2].modelo;
-//                     precio = motos[2].precio;
-                    
-//                 }
-//                 break;
-//             case 2:
-//                 motocross = prompt("¿Qué modelo quéres?\n1- KLX 250L\n2- KX 250F\n3-KX 450F");
-//                 if (motocross == 1) {
-//                     motocross = motos[3].modelo;
-//                     precio = motos[3].precio;
-                    
-//                 } else if (motocross == 2) {
-//                     motocross = motos[4].modelo;
-//                     precio = motos[4].precio;
-                    
-//                 } else if (motocross == 3) {
-//                     motocross = motos[5].modelo;
-//                     precio = motos[5].precio;
-                    
-//                 }
-//                 break;
-//             case 3:
-//                 motocross = prompt("¿Qué modelo quéres?\n1- EXC - F350 $\n2- SX 125\n3-SX 450");
-//                 if (motocross == 1) {
-//                     motocross = motos[6].modelo;
-//                     precio = motos[6].precio;
-                    
-//                 } else if (motocross == 2) {
-//                     motocross = motos[7].modelo;
-//                     precio = motos[7].precio;
-                    
-//                 } else if (motocross == 3) {
-//                     motocross = motos[8].modelo;
-//                     precio = motos[8].precio;
-                    
-//                 }
-//                 break;
-//             case 4:
-//                 motocross = prompt("¿Qué modelo quéres?\n1- RM-Z 250 $\n2- RM-Z 450\n3-RM-Z 450 2017");
-//                 if (motocross == 1) {
-//                     motocross = motos[9].modelo;
-//                     precio = motos[9].precio;
-                    
-//                 } else if (motocross == 2) {
-//                     motocross = motos[10].modelo;
-//                     precio = motos[10].precio;
-                    
-//                 } else if (motocross == 3) {
-//                     motocross = motos[11].modelo;
-//                     precio = motos[11].precio;
-                    
-//                 }
-//                 break;
-//             case 5: 
-//                 motocross = prompt("¿Qué modelo quéres?\n1- WR 450 $\n2- YZ 250\n3- YZ 450");
-//                 if (motocross == 1) {
-//                     motocross = motos[12].modelo;
-//                     precio = motos[12].precio;
-                    
-//                 } else if (motocross == 2) {
-//                     motocross = motos[13].modelo;
-//                     precio = motos[13].precio;
-                    
-//                 } else if (motocross == 3) {
-//                     motocross = motos[14].modelo;
-//                     precio = motos[14].precio;
-                    
-//                 }
-//                 break;
-//             default:
-//                 alert("Alguno de los datos no es correcto")
-//         }
-        
-//         confirm(`Presiona "Aceptar" para confirmar tu compra\nMoto: ${motocross}\nPrecio: $${precio}`)
-//     } while (repetir);
-// }
-
-// let cuotas = 0;
-// let precioDeCuota = 0;
-// const cuantasCuotas = () => {
-//     let quiereCuotas = confirm("¿Desea pagar su moto en cuotas?")
-//     if (quiereCuotas) {
-//         while (cuotas == "" || cuotas > 18 || cuotas <= 0) {
-//             cuotas = parseInt(prompt("¿En cuantas cuotas desea pagar? El maximo es de 18 cuotas (Todas las cuotas tienen un interes)"))
-//             precioDeCuota = precio/cuotas;
-//             console.log(cuotas);
-//             console.log(precioDeCuota);
-//         }
-//     }
-// }
-// let interes;
-// const intereses = (cuotas) => {
-//     if (cuotas == 1){
-//     return 0;
-//     } else {
-//         interes = 0.10*precio;
-//         precioDeCuota = interes+precioDeCuota;
-//         console.log(precioDeCuota);
-//         console.log(interes);
-//     }
-// }
-// const precioFinal = () => {
-//     total = ((precio+interes)/cuotas);
-//     console.log(total);
-//     alert(`El precio final es de $${precio} a abonar en ${cuotas} cuotas de $${precioDeCuota}`);
-
-
-// }
-
-// compraDeMoto();
-// cuantasCuotas();
-// intereses();
-// precioFinal();
